bac3cca53920439df816cd265eb51bdfee44a989
Michal Burda <michal.burda@osu.cz>
2022-10-26 13:07:08 +0200

    Added the running argument to which_runnable()

--- R/which_runnable.R
+++ R/which_runnable.R
@@ -2,13 +2,25 @@
 #' @return
 #' @author Michal Burda
 #' @export
-which_runnable <- function(job) {
+which_runnable <- function(job,
+                           running = rep(FALSE, length(job))) {
     assert_that(is_job(job))
+    assert_that(is.vector(running) && is.logical(running) && length(running) == length(job))
 
     prereq <- prerequisites(job) > 0
     invalid <- which_invalid(job)
+
+    running_prereq <- matrix(running, nrow = 1)
+    while (TRUE) {
+        old <- running_prereq
+        running_prereq <- (running_prereq + running_prereq %*% prereq) > 0
+        if (all(old == running_prereq))
+            break
+    }
+    running_prereq <- as.vector(running_prereq)
+
     valid_prereq <- (invalid %*% prereq) == 0
     valid_prereq <- as.vector(valid_prereq)
 
-    invalid & valid_prereq
+    invalid & !running & !running_prereq & valid_prereq
 }
--- tests/testthat/test-which_runnable.R
+++ tests/testthat/test-which_runnable.R
@@ -12,18 +12,29 @@ test_that("which_runnable", {
     job <- list(a1, a2, a3, a4)
 
     expect_equal(which_runnable(job), c(T, F, F, F))
+    expect_equal(which_runnable(job, c(T, F, F, F)), c(F, F, F, F))
 
     file.create(f1)
     Sys.setFileTime(f1, "1990-01-01")
     expect_equal(which_runnable(job), c(F, T, F, F))
+    expect_equal(which_runnable(job, c(T, F, F, F)), c(F, F, F, F))
+    expect_equal(which_runnable(job, c(F, T, F, F)), c(F, F, F, F))
+    expect_equal(which_runnable(job, c(T, T, F, F)), c(F, F, F, F))
 
     file.create(f2)
     Sys.setFileTime(f2, "1990-01-02")
     expect_equal(which_runnable(job), c(F, F, T, F))
+    expect_equal(which_runnable(job, c(T, F, F, F)), c(F, F, F, F))
+    expect_equal(which_runnable(job, c(F, T, F, F)), c(F, F, F, F))
+    expect_equal(which_runnable(job, c(F, F, T, F)), c(F, F, F, F))
+    expect_equal(which_runnable(job, c(T, T, T, F)), c(F, F, F, F))
 
     file.create(f4)
     Sys.setFileTime(f4, "1990-01-04")
     expect_equal(which_runnable(job), c(F, F, T, F))
+    expect_equal(which_runnable(job, c(T, F, F, F)), c(F, F, F, F))
+    expect_equal(which_runnable(job, c(F, T, F, F)), c(F, F, F, F))
+    expect_equal(which_runnable(job, c(F, F, T, F)), c(F, F, F, F))
 
     file.create(f3)
     Sys.setFileTime(f3, "1990-01-03")
@@ -31,6 +42,10 @@ test_that("which_runnable", {
 
     Sys.setFileTime(f3, "1990-01-05")
     expect_equal(which_runnable(job), c(F, F, F, T))
+    expect_equal(which_runnable(job, c(T, F, F, F)), c(F, F, F, F))
+    expect_equal(which_runnable(job, c(F, T, F, F)), c(F, F, F, F))
+    expect_equal(which_runnable(job, c(F, F, T, F)), c(F, F, F, F))
+    expect_equal(which_runnable(job, c(F, F, F, T)), c(F, F, F, F))
 })
 
 
@@ -48,10 +63,16 @@ test_that("which_runnable 2", {
     job <- list(a1, a2, a3, a4)
 
     expect_equal(which_runnable(job), c(T, T, F, F))
+    expect_equal(which_runnable(job, c(T, F, F, F)), c(F, T, F, F))
+    expect_equal(which_runnable(job, c(T, T, F, F)), c(F, F, F, F))
 
     file.create(f1)
     Sys.setFileTime(f1, "1990-01-01")
     expect_equal(which_runnable(job), c(F, T, T, F))
+    expect_equal(which_runnable(job, c(T, T, F, F)), c(F, F, F, F))
+    expect_equal(which_runnable(job, c(F, T, F, F)), c(F, F, T, F))
+    expect_equal(which_runnable(job, c(F, F, T, F)), c(F, T, F, F))
+    expect_equal(which_runnable(job, c(F, T, T, F)), c(F, F, F, F))
 
     file.create(f2)
     Sys.setFileTime(f2, "1990-01-02")
