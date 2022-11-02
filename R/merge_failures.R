70ab6bc91f225ad91bfcbc0e10195309902937d2
Michal Burda <michal.burda@osu.cz>
2022-11-02 18:59:59 +0100

    merge_failures() now leave only the last message

--- R/merge_failures.R
+++ R/merge_failures.R
@@ -6,5 +6,10 @@ merge_failures <- function(...) {
     dots <- list(...)
     assert_that(all(sapply(dots, is.list)))
 
-    do.call(mapply, c(list(c), dots))
+    do.call(mapply, c(list(.mergefun), dots))
+}
+
+
+.mergefun <- function(...) {
+    tail(c(...), 1)
 }
--- tests/testthat/test-merge_failures.R
+++ tests/testthat/test-merge_failures.R
@@ -5,7 +5,5 @@ test_that("merge_failures", {
     expect_equal(merge_failures(list(NULL, 'a', 'b'),
                                 list(NULL, 'x', NULL),
                                 list(NULL, 'z', NULL)),
-                 list(NULL,
-                      c('a', 'x', 'z'),
-                      'b'))
+                 list(NULL, 'z', 'b'))
 })
