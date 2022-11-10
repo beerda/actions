444ab12418d55fe86f907614277520c86f14fe5f
Michal Burda <michal.burda@osu.cz>
2022-11-10 20:42:58 +0100

    Enhanced build status messages

--- R/build.R
+++ R/build.R
@@ -6,6 +6,11 @@ build <- function(job, processes = 1, timeout = 10000) {
     assert_that(is_job(job))
     assert_that(is.count(processes))
 
+    cat(rule(left = paste0(style_bold('Build started'), ' (', Sys.time(), ')'),
+             line = 2,
+             col = col_white), '\n')
+    print(job)
+
     job <- init_build(job)
     job <- mark_missing_deps(job)
     job <- mark_failed_deps(job)
@@ -15,6 +20,7 @@ build <- function(job, processes = 1, timeout = 10000) {
     runnable_actions <- which_runnable(job) & !finished_actions
     workers <- create_working_group(processes)
 
+    start_time <- Sys.time()
     while (any(runnable_actions) || length(workers) > 0) {
         workers <- dispatch(workers, job, runnable_actions)
         res <- poll(workers, timeout)
@@ -30,6 +36,15 @@ build <- function(job, processes = 1, timeout = 10000) {
         runnable_actions <- which_runnable(job, res$running_actions | failed_actions)
         runnable_actions <- runnable_actions & !finished_actions & !res$running_actions
     }
+    end_time <- Sys.time()
+    diff_time <- as.numeric(end_time - start_time)
+    diff_time <- round(diff_time, 1)
 
-    print(props(job, 'status'))
+    cat(rule(left = style_bold('Build summary'),
+             line = 2,
+             col = col_white), '\n')
+    print(job)
+    cat(rule(left = paste0(style_bold('Build finished'), ' (', diff_time, ' s)'),
+             line = 2,
+             col = col_white), '\n')
 }
--- R/print-job.R
+++ R/print-job.R
@@ -5,26 +5,51 @@
 print.job <- function(job, ...) {
     assert_that(is_job(job, FALSE))
 
+
     no <- as.character(seq_along(job))
     deps <- .format(job, 'depends', function(d) d[-length(d)])
     script <- .format(job, 'depends', function(d) d[length(d)])
     targets <- .format(job, 'targets', identity)
 
-    obsolete <- Vectorize(is_obsolete)(job)
     cols <- prepare_table(list(no, deps, script, targets),
                           min_size = 12,
-                          total_size = console_width() - 3 * 3)
+                          total_size = console_width() - 3 * 3 - 2)
 
+    init <- attr(job, 'init_build')
+    if (is.null(init))
+        init <- FALSE
 
-    for (i in seq_along(job)) {
-        color <- if (obsolete[i]) col_br_white else col_grey
+    if (init) {
+        failed <- unlist(props(job, 'failed'))
+        finished <- unlist(props(job, 'finished'))
+        icon <- ifelse(finished,
+                       ifelse(failed, '✖ ', '✔ '),
+                       '  ')
+        for (i in seq_along(job)) {
+            color <- if (finished[i]) if (failed[i]) col_br_red else col_br_green else col_white
+            cat(color(icon[i]))
+            cat(col_br_white('[', cols[[1]][i], '] '))
+            cat(col_br_white(cols[[2]][i]))
+            cat(col_br_white(' ▶ '))
+            cat(color(cols[[3]][i]))
+            cat(col_br_white(' ▶ '))
+            cat(col_br_white(cols[[4]][i], '\n'))
+        }
 
-        cat(col_br_white('[', cols[[1]][i], '] '))
-        cat(color(cols[[2]][i]))
-        cat(col_br_white(' ▶ '))
-        cat(color(cols[[3]][i]))
-        cat(col_br_white(' ▶ '))
-        cat(color(cols[[4]][i], '\n'))
+    } else {
+        obsolete <- Vectorize(is_obsolete)(job)
+        icon <- ifelse(obsolete, '• ', '  ')
+        for (i in seq_along(job)) {
+            color <- if (obsolete[i]) col_br_green else col_white
+
+            cat(color(icon[i]))
+            cat(col_br_white('[', cols[[1]][i], '] '))
+            cat(col_br_white(cols[[2]][i]))
+            cat(col_br_white(' ▶ '))
+            cat(color(cols[[3]][i]))
+            cat(col_br_white(' ▶ '))
+            cat(col_br_white(cols[[4]][i], '\n'))
+        }
     }
 }
 
